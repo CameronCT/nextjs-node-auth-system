80f480036478fbb141118eadd593ed3f6b50b32c
Cameron <CameronCT@users.noreply.github.com>
2022-07-11 21:04:31 -0700

    feat: prettier on oauth

--- server/src/oauth.ts
+++ server/src/oauth.ts
@@ -1,35 +1,35 @@
-import Config from "./config";
-import dotenv from "dotenv";
-dotenv.config();
+import Config from './config'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export interface OAuthParameters {
-  Client: string;
-  Secret: string;
-  Callback: string;
+    Client: string
+    Secret: string
+    Callback: string
 }
 
 export interface OAuth {
-  Discord: OAuthParameters;
-  Google: OAuthParameters;
-  GitHub: OAuthParameters;
+    Discord: OAuthParameters
+    Google: OAuthParameters
+    GitHub: OAuthParameters
 }
 
 const payload: OAuth = {
-  Discord: {
-    Client: "123",
-    Secret: String(process.env.OAUTH_DISCORD || ''),
-    Callback: `${Config.api.oauthUrl}/auth/discord/callback`,
-  },
-  Google: {
-    Client: "123",
-    Secret: String(process.env.OAUTH_GOOGLE || ''),
-    Callback: `${Config.api.oauthUrl}/auth/google/callback`,
-  },
-  GitHub: {
-    Client: "123",
-    Secret: String(process.env.OAUTH_GITHUB || ''),
-    Callback: `${Config.api.oauthUrl}/auth/github/callback`,
-  }
-};
+    Discord: {
+        Client: '123',
+        Secret: String(process.env.OAUTH_DISCORD || ''),
+        Callback: `${Config.api.oauthUrl}/auth/discord/callback`,
+    },
+    Google: {
+        Client: '123',
+        Secret: String(process.env.OAUTH_GOOGLE || ''),
+        Callback: `${Config.api.oauthUrl}/auth/google/callback`,
+    },
+    GitHub: {
+        Client: '123',
+        Secret: String(process.env.OAUTH_GITHUB || ''),
+        Callback: `${Config.api.oauthUrl}/auth/github/callback`,
+    },
+}
 
-export default payload;
+export default payload
